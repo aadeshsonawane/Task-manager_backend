a06cc8e834ac3dc99ff9e3e9991c299b0740a0f8
aadeshsonawane <aadeshsonawane307@gmail.com>
2026-06-04 16:55:22 +0530

    create server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server running on port ${process.env.PORT || 5000}`);
+});
