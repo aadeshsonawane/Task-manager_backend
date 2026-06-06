8135fd754a8c1831eccb88ef1d0727bb08145f64
aadeshsonawane <aadeshsonawane307@gmail.com>
2026-06-06 11:28:24 +0530

    update server.js file

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
 
+
 connectDB();
+
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
+
 
 app.get('/', (req, res) => {
   res.send('API is running...');
